6927f91c04ae3b8461dddcf0cf1c02d11f37917c
Daniel Vopalecky <daniel.vopalecky@gmail.com>
2024-01-24 23:56:15 +0100

    Add prettier

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Svelte starter / boilerplate project
 
 ## Technologies used
-- svelte
-- bun
-- rsbuild (using rspack)
-- tailwind
-- eslint
+- [Svelte](https://svelte.dev/)
+- [Bun](https://bun.sh/)
+- [Rsbuild (using rspack)](http://rsbuild.dev)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [ESLint](https://eslint.org/)
+- [Prettier](https://prettier.io/)
 
 ## List of commands
 - `bun install`
--- src/App.svelte
+++ src/App.svelte
@@ -10,23 +10,23 @@
 </main>
 
 <style>
-.content {
-  display: flex;
-  min-height: 100vh;
-  line-height: 1.1;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-}
+  .content {
+    display: flex;
+    min-height: 100vh;
+    line-height: 1.1;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+  }
 
-.content h1 {
-  font-size: 3.6rem;
-  font-weight: 700;
-}
+  .content h1 {
+    font-size: 3.6rem;
+    font-weight: 700;
+  }
 
-.content p {
-  font-size: 1.2rem;
-  font-weight: 400;
-  opacity: 0.5;
-}
+  .content p {
+    font-size: 1.2rem;
+    font-weight: 400;
+    opacity: 0.5;
+  }
 </style>
